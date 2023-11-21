3beaed21b64986c67b0d16e5329965ec5fe120fb
javier <kuznikjavier@gmail.com>
2023-11-21 08:24:40 -0300

    actividad 6: contenedores

---
+++ todoListCrudSBDocker/src/main/java/com/example/todoListCrudSB/TodoListCrudSbApplication.java
@@ -0,0 +1,12 @@
+package com.example.todoListCrudSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListCrudSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListCrudSbApplication.class, args);
+	}
+}
